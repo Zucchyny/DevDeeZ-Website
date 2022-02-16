716c01032de3b9e742450889e6cde6889c407fff
Zucchyny <tristan.fourot@outlook.fr>
2022-02-16 18:54:39 +0100

    loader-commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 // here you put all the js you want.
+import Loader from "./js/loader.js";
 import Navbar from "./js/navbar.js";
 import Beaver from "./js/beaver.js";
 import Carousel from "./js/carousel.js";
 
+const loader = new Loader();
 const navbar = new Navbar();
 const beaver = new Beaver();
 const carousel = new Carousel();
